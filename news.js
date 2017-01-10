785e34d689f817dfbe2cd92492883323c7630251
Wil <wfkoch@gmail.com>
2017-01-10 15:29:54 -0500

    Add example format, move personal styles here

---
+++ news.js
@@ -0,0 +1,8 @@
+var announce = [
+	{
+		"description": 'Oh Snap!'
+	},
+	{
+		"description": '"What is this? A center for ants? How can we be expected to teach children to learn how to read... if they cant even fit inside the building?" --Derek Zoolander'
+	}
+]
